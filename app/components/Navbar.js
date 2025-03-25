41b41439abd2ee320fd415f57ca8b81271326b56
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-25 23:11:41 +0530

    initial work

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=' p-2'>
-      <div className='font-bold text-4xl text-purple-700'>
+    <div className=' p-2 bg-gray-800 flex justify-between'>
+      <div className='font-extrabold text-2xl text-purple-700 '>
         CVI Hub
+      </div>
+      <div>
+        <ul className='flex gap-5'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+
       </div>
     </div>
   )
