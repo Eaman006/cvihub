4de0a67eaad12cd74b5824e034cda1329520106d
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-25 23:03:16 +0530

    Under Construction

---
+++ app/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' p-2'>
+      <div className='font-bold text-4xl text-purple-700'>
+        CVI Hub
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
