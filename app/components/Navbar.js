a19dbeda742a60295640b7ae4a7cfcb734adfc33
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-27 20:39:31 +0530

    add logo image to navbar for improved branding

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className=' p-2 bg-gray-800 flex justify-between'>
-      <div className='font-extrabold text-3xl text-purple-400 '>
+      <div className='font-extrabold text-3xl text-purple-400 flex items-center'>
+        <Image src="/comp.gif" width={70} height={40} alt='top logo' />
+        <div>
         CVI Hub
+        </div>
       </div>
       <div>
         
