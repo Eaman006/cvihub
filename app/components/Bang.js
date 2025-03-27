62ba5186c7b15a98dfb778355802fa82ed709798
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-27 21:59:29 +0530

    update Bang component layout and add server GIF for enhanced visual appeal

--- app/components/Bang.js
+++ app/components/Bang.js
@@ -4,10 +4,13 @@ import Image from 'next/image'
 const Bang = () => {
     return (
         <div className=''>
-            <div>
-                
+            <div className='flex justify-center items-center'>
+            <div className='bg-[#0000005b] rounded-full px-7 py-7'>
+                <Image src="/comp.gif" width={200} height={200} alt='aws' />
             </div>
-            <div className='flex justify-evenly'>
+            
+            </div>
+            <div className='flex justify-evenly m-2 p-2'>
                 <div className='bg-[#0000005b] rounded-full px-2 py-7'>
                     <Image src="/aws.png" width={100} height={100} alt='aws' />
                 </div>
