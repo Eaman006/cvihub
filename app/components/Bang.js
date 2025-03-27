19ffa9d68e69707e312dd011857c6a3106eed576
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-03-27 22:45:24 +0530

    enhance Bang component layout with connecting lines and improved icon container

--- app/components/Bang.js
+++ app/components/Bang.js
@@ -3,25 +3,35 @@ import Image from 'next/image'
 
 const Bang = () => {
     return (
-        <div className=''>
-            <div className='flex justify-center items-center'>
-            <div className='bg-[#0000005b] rounded-full px-7 py-7'>
-                <img src="/comp.gif" width={200} height={200} alt='aws' />
+        <div className='relative'>
+            {/* Center image */}
+            <div className='flex justify-center items-center relative'>
+                <div className='bg-[#0000005b] rounded-full px-7 py-7 relative z-10'>
+                    <img src="/comp.gif" width={200} height={200} alt='computer' />
+                </div>
             </div>
+            {/* Horizontal connecting line */}
+            <div className="absolute top-56 left-1/4 w-1/2 h-0.5 bg-gray-400"></div>
+
+            {/* Vertical lines */}
+            <div className="absolute top-52 left-1/4 w-0.5 h-24 bg-gray-400"></div>
+            <div className="absolute top-28 left-1/2 w-0.5 h-64 bg-gray-400"></div>
+            <div className="absolute top-52 left-3/4 w-0.5 h-24 bg-gray-400"></div>
+
+            {/* Horizontal connecting line */}
             
-            </div>
-            <div className='flex justify-evenly m-2 p-2'>
-                <div className='bg-[#0000005b] rounded-full px-2 py-7'>
+
+            {/* Container for icons */}
+            <div className='flex justify-evenly items-start px-2 pt-24 relative'>
+                <div className='bg-[#0000005b] rounded-full p-5 relative z-10'>
                     <Image src="/aws.png" width={100} height={100} alt='aws' />
                 </div>
-                <div className='bg-[#0000005b] rounded-full px-7 py-7 '>
-                    <Image src="/git.png" width={60} height={60} alt='aws' />
+                <div className='bg-[#0000005b] rounded-full p-5 relative z-10'>
+                    <Image src="/git.png" width={60} height={60} alt='git' />
                 </div>
-                <div className='bg-[#0000005b] rounded-full px-7 py-7 '>
-                    <Image src="/gcp.png" width={60} height={60} alt='aws' />
-                </div>             
-
-
+                <div className='bg-[#0000005b] rounded-full p-5 relative z-10'>
+                    <Image src="/gcp.png" width={60} height={60} alt='gcp' />
+                </div>
             </div>
         </div>
     )
